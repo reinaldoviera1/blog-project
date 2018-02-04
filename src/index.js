ce0ea426abd5713463d9313e8cac0ead4e290ef2
reinaldoviera1 <reinaldo.viera1@gmail.com>
2018-02-04 17:20:57 +0100

    App mount point.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // Set up your application entry point here...
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+const store = configureStore();
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+     document.getElementById('app')
+);
